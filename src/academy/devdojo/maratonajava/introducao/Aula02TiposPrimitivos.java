6456a9eb14319903f55d1988d440db5cbc18034e
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-12 12:01:32 -0300

    adiciona alguns exercicios

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int idade = 29;
+        long numeroLong = 1000000L;
+        double salario = 5000;
+        float salarioFloat = 2500F;
+        byte idadeByte = 10;
+        short idadeShort = 9;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char sexo = 'M';
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "João Lima";
+        double salario = 3500;
+        String endereco = "Rua José Hortêncio de Souza, N32";
+        String data = "20/01/2026";
+        System.out.println("Eu " + nome+ ", morando no endereço "+ endereco + ", confirmo"
+        + " que recebi o salario de R$ " + salario + ", nada data "+ data
+        );
+    }
+}
