cf4d4f19642aab97f4fb41eb73b698fc18ade592
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-13 20:06:24 -0300

    aula 05

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+      int idade = 20;
+
+      String decisao = idade > 18 ? "pau torando" : "pau nao torando";
+
+      System.out.println(decisao);;
+    }
+}
