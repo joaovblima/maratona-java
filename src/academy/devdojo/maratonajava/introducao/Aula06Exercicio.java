e7c74bf5837c74c30978e0be0fa0ec6873c4bfc3
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-14 10:24:42 -0300

    addd pequena alteração

--- src/academy/devdojo/maratonajava/introducao/Aula06Exercicio.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06Exercicio.java
@@ -3,16 +3,18 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula06Exercicio {
     public static void main(String[] args) {
         double salarioAnual = 34711;
-        double taxa = 9.7;
+        double primeiraFaixa = 9.7/100;
+        double segundaFaixa = 37.35/100;
+        double terceiraFaixa = 49.5/100;
         double valorDesconto;
         if (salarioAnual <= 34712) {
-            valorDesconto = salarioAnual * (taxa / 100);
-        } else if (salarioAnual <= 68507) {
-            taxa = 37.35/100;
-            valorDesconto = salarioAnual * taxa;
+            valorDesconto = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual>= 34713 && salarioAnual <= 68507) {
+
+            valorDesconto = salarioAnual * segundaFaixa;
         } else {
-            taxa = 49.5/100;
-            valorDesconto = salarioAnual * taxa;
+
+            valorDesconto = salarioAnual * terceiraFaixa;
         }
         System.out.printf("valor a pagar de taxa = R$ %.2f",  valorDesconto);
     }
