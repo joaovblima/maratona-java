f027cc3c38f8ae60fca519d38f3113de2d7b344a
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-14 14:37:13 -0300

    add exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasDeRepeticao02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        //dado um valor de um carro descubra quantas vezes ele pode ser parcelado
+        // valor da parcela >= 1000;
+        double valorTotal = 30000;
+
+
+        for (int parcela = 1; parcela <= valorTotal; parcela++ ) {
+            double valorParcela = valorTotal / parcela;
+
+            if (valorParcela >= 1000) {
+                System.out.println("Parcela" + parcela+ "+ valor R$"+ valorParcela);
+            }
+        }
+    }
+}
--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasDeRepeticao {
+public class Aula06EstruturasDeRepeticao {
     public static void main(String[] args) {
 
         for (int i = 0; i< 1000000; i++) {
