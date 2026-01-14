02812bc6eafca7993fbe34182669bca7e71539b2
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-14 15:00:30 -0300

    ex de com arrays

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Arrays02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Arrays02 {
+    public static void main(String[] args) {
+        String [] nomes = new String[3];
+        nomes[0] = "Naruto";
+        nomes[1] = "Sasuke";
+        nomes[2] = "Sakura";
+        nomes[3] = "Kakashi";
+
+
+    }
+}
--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -1,4 +1,11 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int [] idades = {1, 20, 30, 40};
+
+        for (int i = 0; i <idades.length; i++) {
+            System.out.println(idades[i]);
+        }
+    }
 }
