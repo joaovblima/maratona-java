ba0f846d0c83a9e107a31be6a02ceab88ad78fbf
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-15 22:27:01 -0300

    introdução a classes

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTeste01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "João";
+        estudante.idade = 29;
+        estudante.sexo = 'M';
+
+
+    }
+}
