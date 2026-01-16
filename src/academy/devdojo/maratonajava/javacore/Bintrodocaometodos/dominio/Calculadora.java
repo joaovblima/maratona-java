b918dea5626161666798b7bc03f9a413172dd46f
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-15 22:57:25 -0300

    introducao a metodos com a classe Calculadora

---
+++ src/academy/devdojo/maratonajava/javacore/Bintrodocaometodos/dominio/Calculadora.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Bintrodocaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println(10 + 20);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintrodocaometodos/test/CalculadoraTeste01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintrodocaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintrodocaometodos.dominio.Calculadora;
+
+public class CalculadoraTeste01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+
+    }
+}
