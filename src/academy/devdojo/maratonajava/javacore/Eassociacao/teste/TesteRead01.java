a02dd743fc29a1648535747e024efb9ca111d161
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-18 21:19:16 -0300

    adiciona um simples teste de Scanner

---
+++ src/academy/devdojo/maratonajava/javacore/Eassociacao/teste/TesteRead01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Eassociacao.teste;
+
+import java.util.Scanner;
+
+public class TesteRead01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome = sc.nextLine();
+        System.out.println(nome);
+
+
+
+        sc.close();
+
+    }
+}
