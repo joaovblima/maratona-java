61210e299cf7415d5547b7a9559d6ff1618fef97
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-20 18:36:30 -0300

    adiciona novo teste

---
+++ src/academy/devdojo/maratonajava/javacore/Fheranca/teste/HerancaTeste03.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Fheranca.teste;
+
+public class HerancaTeste03 {
+    public static void main(String[] args) {
+        System.out.println("teste teste ");
+    }
+}
