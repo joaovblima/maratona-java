69a0ceb684ae3167c42e18f026e377b16fc06b21
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-21 11:13:51 -0300

    adiciona exemplos utilizando constantes

---
+++ src/academy/devdojo/maratonajava/javacore/Hmodificadorfinal/teste/CarroTeste01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Hmodificadorfinal.teste;
+
+import academy.devdojo.maratonajava.javacore.Hmodificadorfinal.dominio.Carro;
+
+public class CarroTeste01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.setNome("Hilux");
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+
+
+
+    }
+}
