ef4dc899eead630af6c1b0a9916e8c196532ba73
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-21 11:03:52 -0300

    pequena revisão que trata sobrescrita de métodos

---
+++ src/academy/devdojo/maratonajava/javacore/Gsobrescrita/dominio/Filme.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gsobrescrita.dominio;
+
+public class Filme {
+    private String nome;
+
+    public Filme(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
