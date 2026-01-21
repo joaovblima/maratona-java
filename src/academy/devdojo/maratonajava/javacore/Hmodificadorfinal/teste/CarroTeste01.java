58fdfae38a705fcb4a8e4c0d46382f6af0dbc5db
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-21 11:21:25 -0300

    adiciona exemplo de modificador final utilizando uma classe

--- src/academy/devdojo/maratonajava/javacore/Hmodificadorfinal/teste/CarroTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Hmodificadorfinal/teste/CarroTeste01.java
@@ -1,12 +1,16 @@
 package academy.devdojo.maratonajava.javacore.Hmodificadorfinal.teste;
 
 import academy.devdojo.maratonajava.javacore.Hmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Hmodificadorfinal.dominio.Comprador;
 
 public class CarroTeste01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         carro.setNome("Hilux");
         System.out.println(Carro.VELOCIDADE_LIMITE);
+
+        carro.COMPRADOR.setNome("João");
+        System.out.println(carro.COMPRADOR);
 
 
 
