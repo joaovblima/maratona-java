16fea41ea0717aa417836133476aef2e10c1e05f
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-24 19:36:56 -0300

    utiliza de forma correta CalculadoraImposto

--- src/academy/devdojo/maratonajava/javacore/Lpolimorfismo/teste/ProdutoTeste02.java
+++ src/academy/devdojo/maratonajava/javacore/Lpolimorfismo/teste/ProdutoTeste02.java
@@ -2,13 +2,17 @@ package academy.devdojo.maratonajava.javacore.Lpolimorfismo.teste;
 
 import academy.devdojo.maratonajava.javacore.Lpolimorfismo.dominio.Computador;
 import academy.devdojo.maratonajava.javacore.Lpolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Lpolimorfismo.dominio.Televisao;
+import academy.devdojo.maratonajava.javacore.Lpolimorfismo.servico.CalculadoraImposto;
 
 public class ProdutoTeste02 {
     public static void main(String[] args) {
         Produto produto = new Computador("Positivo", 4000);
+        Televisao tv = new Televisao("Samsung 4k ULTRA WIDE GPT 4.0 60\"", 10000);
 
-        System.out.println(produto.getNome());
-        System.out.println(produto.getValor());
-        System.out.println(produto.calcularImposto());
+
+        CalculadoraImposto.calcularImposto(produto);
+        System.out.println("------------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Lpolimorfismo/teste/ProdutoTeste03.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Lpolimorfismo.teste;
+
+public class ProdutoTeste03 {
+    public static void main(String[] args) {
+
+    }
+}
