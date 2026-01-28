440431d3cd17d4929d16286c71cf967435b9e428
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-28 18:12:59 -0300

    adiciona teste simples

---
+++ src/academy/devdojo/maratonajava/javacore/mexcecoes/teste/TesteQualquer.java
@@ -0,0 +1,6 @@
+package academy.devdojo.maratonajava.javacore.mexcecoes.teste;
+
+public class TesteQualquer {
+    public static void main(String[] args) {
+        System.out.println("que loucura, nao tem como desistir de você meu javinha, vou manter minha promessa. ");    }
+}
