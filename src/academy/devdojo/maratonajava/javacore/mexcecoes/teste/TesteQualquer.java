cbb87146e166a8d5a970fb9b378dc630da89d9f9
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-29 10:59:44 -0300

    fazendo testes com funcoes estaticas

--- src/academy/devdojo/maratonajava/javacore/mexcecoes/teste/TesteQualquer.java
+++ src/academy/devdojo/maratonajava/javacore/mexcecoes/teste/TesteQualquer.java
@@ -2,5 +2,30 @@ package academy.devdojo.maratonajava.javacore.mexcecoes.teste;
 
 public class TesteQualquer {
     public static void main(String[] args) {
-        System.out.println("que loucura, nao tem como desistir de você meu javinha, vou manter minha promessa. ");    }
+        System.out.println("que loucura, nao tem como desistir de você meu javinha, vou manter minha promessa. ");
+        System.out.println("tenho que persistir até conseguir. vamos la. estamos mais da metade, nao podemos parar agora. ");
+        retornaAlgo();
+        System.out.println(somaNumeros(2, 4));
+        saudacao("joao");
+        testandoFuncoesStaticasFinalmente();
+    }
+
+    public static void retornaAlgo() {
+        System.out.println("retorna algo");
+    }
+
+    public static int somaNumeros(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public static void saudacao(String nome) {
+        System.out.println("Ola " + nome + " tu é doido de pedra");
+    }
+
+    public static void testandoFuncoesStaticasFinalmente() {
+        retornaAlgo();
+        int resultado = somaNumeros(10, 30);
+        System.out.println(resultado);
+        saudacao("joao victor bernardino de lima");
+    }
 }
