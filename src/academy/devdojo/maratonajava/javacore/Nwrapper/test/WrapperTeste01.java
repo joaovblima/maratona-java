d6909897bc5e416c9e5d321b4f97d2cecda5f1f6
João Lima <joaovictorbernardinodelima@gmail.com>
2026-01-30 10:17:40 -0300

    introducao a Classes Wrappers

---
+++ src/academy/devdojo/maratonajava/javacore/Nwrapper/test/WrapperTeste01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Nwrapper.test;
+
+public class WrapperTeste01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 1L;
+        float floatP = 1.0F;
+        double doubleP = 1.0;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 1L;
+        Float floatW = 1.0F;
+        Double doubleW = 1.0;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+    }
+}
