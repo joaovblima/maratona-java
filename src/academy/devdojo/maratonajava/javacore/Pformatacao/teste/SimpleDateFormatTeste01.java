259088ababb1109c8d0acd9f3f3a8c7b6a32fb9b
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-02 12:50:58 -0300

    adiciona exemplo simples utilizando SimpleDateFormat

--- src/academy/devdojo/maratonajava/javacore/Pformatacao/teste/SimpleDateFormatTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/Pformatacao/teste/SimpleDateFormatTeste01.java
@@ -1,7 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Pformatacao.teste;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SimpleDateFormatTeste01 {
     public static void main(String[] args) {
-
+        String pattern = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        System.out.println(sdf.format(new Date()));
     }
 }
