e6bbdda911706983072fbf771e60f6a2f5ae3e68
João Lima <joaovictorbernardinodelima@gmail.com>
2026-02-02 11:42:03 -0300

    adiciona um exemplo com a antiga classe Date

---
+++ src/academy/devdojo/maratonajava/javacore/Nwrapper/string/StringBuilderTeste01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Nwrapper.string;
+
+public class StringBuilderTeste01 {
+    public static void main(String[] args) {
+        String nome = "João Lima";
+        nome.concat("vai jl");
+        System.out.println(nome);
+        StringBuilder sb = new StringBuilder(16);
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Odatas/teste/DateTeste01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Odatas.teste;
+
+import java.util.Date;
+
+public class DateTeste01 {
+    public static void main(String[] args) {
+        Date data = new Date(1_000_000_000_000L);
+        System.out.println(data);
+    }
+}
